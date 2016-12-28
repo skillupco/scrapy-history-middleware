a4ff16976b10a2dbfa8df7446e0ead84b0baa96a
cmorent <cedric.morent@dailymotion.com>
2016-12-28 10:46:13 +0100

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -11,6 +11,26 @@
 # invocation. For example:
 #    (venv)$ ./runtests.sh --pdb-failure # will drop into pdb on failure
 
+wrapper=
+for file in "/usr/share/virtualenvwrapper/virtualenvwrapper.sh" "/usr/local/bin/virtualenvwrapper.sh"
+do
+    echo -n "Does \`${file}' exists ? "
+    [ -f "${file}" ] && { wrapper="$file" ; echo "yes" ; } || { echo "no" ; }
+    [ -n "${wrapper}" ] && break
+done
+[ -z "${wrapper}" ] && { echo "Please install virtualenvwrapper" 1>&2 ; exit 1 ; }
+
+export WORKON_HOME="/home/teamcity/.virtualenvs"
+source "${wrapper}"
+workon teamcity 2> /dev/null
+if [ "0" != "$?" ]
+then
+    echo "Please setup a virtualenv named \`teamcity' for this test-runner."
+    exit 1
+fi
+
+pip install --upgrade -r requirements.txt
+
 nosetests \
     --with-doctest \
     --with-cov --cov-report=html --cov-config .coveragerc --cover-erase \
