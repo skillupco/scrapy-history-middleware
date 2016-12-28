c565965fb21624ea47e8ffb6b059eafe63a6bb96
cmorent <cmorent@dailymotion.com>
2016-12-28 11:16:29 +0100

    Fix issues with packaging

--- history/__init__.py
+++ history/__init__.py
@@ -2,7 +2,7 @@
 """
 
 __title__ = 'history'
-__version__ = '0.9.1'
+__version__ = '0.9.2'
 
 
 #from history.middleware import HistoryMiddleware
--- setup.py
+++ setup.py
@@ -17,16 +17,16 @@ requires = [
 ]
 
 setup(
-    name='history',
+    name='scrapyhistory',
     version=history.__version__,
     description='Scrapy downloader middleware to enable persistent storage.',
-    long_description=open('README.md').read(),
+    #long_description=open('README.md').read(),
     author='Andrew Preston',
     author_email='andrew@preston.co.nz',
     url='http://github.com/playandbuild/scrapy-history-middleware',
     packages=packages,
     install_requires=requires,
-    license=open('LICENSE').read(),
+    #license=open('LICENSE').read(),
     classifiers=(
         'Development Status :: 4 - Beta'
         'Intended Audience :: Developers',
